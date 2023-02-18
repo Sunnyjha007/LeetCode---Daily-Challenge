12ee9034f18566495ccb38d054b1a14940ea7334
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2023-02-18 08:20:11 +0530

    Create Day_18_Invert Binary Tree.cpp

---
+++ February/Day_18_Invert Binary Tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)return NULL;
+        TreeNode* temp = root->left;
+        root->left=root->right;
+        root->right = temp;
+        if(root->left)
+        invertTree(root->left);
+        if(root->right)
+        invertTree(root->right);
+        return root;
+        
+        
+    }
+};
